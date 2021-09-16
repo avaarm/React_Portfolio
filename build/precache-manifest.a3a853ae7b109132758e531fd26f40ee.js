c195ae92a1067b8992403025d918495377618aea
avaarm <avaarm95@gmail.com>
2021-09-16 15:36:23 -0700

    deploy

---
+++ build/precache-manifest.a3a853ae7b109132758e531fd26f40ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f70319433e74b64446412405fc99d3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8fb31a6e97749b83cb66",
+    "url": "/react-portfolio/static/css/main.85db82e4.chunk.css"
+  },
+  {
+    "revision": "2c950052e724ada334f9",
+    "url": "/react-portfolio/static/js/2.1e1fb0d8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.1e1fb0d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb31a6e97749b83cb66",
+    "url": "/react-portfolio/static/js/main.7dfaf13a.chunk.js"
+  },
+  {
+    "revision": "7caf196807214d0a115d",
+    "url": "/react-portfolio/static/js/runtime-main.c7c28671.js"
+  },
+  {
+    "revision": "7d654229444a65f6501da207271c87ef",
+    "url": "/react-portfolio/static/media/background.7d654229.jpg"
+  },
+  {
+    "revision": "a8f8edf37ed3d60ef968138bb1b4863c",
+    "url": "/react-portfolio/static/media/betterEarth.a8f8edf3.png"
+  },
+  {
+    "revision": "f570d6f06933c32b4e7431319f71e3bc",
+    "url": "/react-portfolio/static/media/budgetTracker.f570d6f0.png"
+  },
+  {
+    "revision": "e8f86efd1a9ffa4f97f7f9d25cb6249c",
+    "url": "/react-portfolio/static/media/devBun.e8f86efd.png"
+  },
+  {
+    "revision": "1fe57bb0f82bfd55dd161f996032801e",
+    "url": "/react-portfolio/static/media/healthFrontiers.1fe57bb0.png"
+  },
+  {
+    "revision": "ca46fbf8bf6a150810d0cb7810d3454c",
+    "url": "/react-portfolio/static/media/uplift.ca46fbf8.png"
+  },
+  {
+    "revision": "25d4daf6eb41a37cd887d64b39ed9f10",
+    "url": "/react-portfolio/static/media/workoutTracker.25d4daf6.png"
+  }
+]);
